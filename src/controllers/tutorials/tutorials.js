b6016133d9b960c8446b615a73ebc8839b9cc927
erschm240 <schmitze608@gmail.com>
2026-03-09 15:47:59 -0600

    feat: changed logo color, added model function that separates each tutorial paragraph.

--- src/controllers/tutorials/tutorials.js
+++ src/controllers/tutorials/tutorials.js
@@ -1,4 +1,4 @@
-import { getAllTutorials, getTutorialBySlug } from '../../models/tutorials/tutorials.js';
+import { getAllTutorials, getTutorialBySlug, separateTutorialParagraphs } from '../../models/tutorials/tutorials.js';
 
 // Route handler for tutorial list page
 const tutorialListPage = (req, res) => {
@@ -12,7 +12,11 @@ const tutorialListPage = (req, res) => {
 // Route handler for tutorial details page
 const tutorialDetailPage = (req, res, next) => {
     const tutorialSlug = req.params.tutorialSlug;
+
     const tutorial = getTutorialBySlug(tutorialSlug);
+
+    const tutorialParagraphs = separateTutorialParagraphs(tutorialSlug);
+
     // If the tutorial doesn't exist
     if (!tutorial) {
         const err = new Error(`Tutorial ${tutorialSlug} not found`);
@@ -22,7 +26,8 @@ const tutorialDetailPage = (req, res, next) => {
 
     res.render('tutorials/details', {
         title: tutorial.title,
-        tutorial: {...tutorial}
+        tutorial: {...tutorial},
+        paragraphs: tutorialParagraphs
     });
 };
 
--- src/models/tutorials/tutorials.js
+++ src/models/tutorials/tutorials.js
@@ -1,19 +1,19 @@
 const tutorials = {
-    'cast-on': {
+    'cast-on-knit': {
         id: 0,
-        slug: 'cast-on',
+        slug: 'cast-on-knit',
         title: 'Casting On for Knitting',
-        description: 'Learn how to cast on to start your new knitting project.',
+        description: 'Learn how to cast on to start your knitting project.',
         textContent: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a mauris quis libero elementum pretium sit amet eu justo. Pellentesque id mauris ac nisi euismod laoreet. Morbi velit ligula, imperdiet ut sem vel, condimentum placerat arcu. Sed ligula dolor, venenatis a aliquet nec, molestie nec eros. Praesent tempor nulla ut tincidunt ultricies. Nunc mollis facilisis massa, quis ullamcorper nisi iaculis vitae. Nunc risus magna, fringilla eleifend leo eu, pulvinar laoreet leo.\nIn vel enim mi. Cras facilisis enim eu tempor porta. Sed malesuada elit non imperdiet semper. Duis quis enim elementum, tempus ipsum eget, finibus dolor. Phasellus vulputate condimentum purus, et elementum turpis vehicula et. Phasellus quis tempus risus, ut facilisis libero. Donec rhoncus enim interdum erat vulputate ultrices. Cras dapibus sed nisl suscipit bibendum. Etiam id elit ut ex feugiat pretium et sit amet felis. Nunc auctor vestibulum est, ac ultrices elit rutrum at. Etiam sit amet dui massa. Nam maximus convallis lacinia.\nSuspendisse sed quam nec lectus porta egestas lobortis non ante. Quisque aliquam sapien vel magna dictum sollicitudin. Vivamus tincidunt orci id sapien placerat, at pellentesque mauris venenatis. Phasellus in imperdiet mi. Integer ultrices velit non dapibus congue. Praesent porta rutrum feugiat. Nulla sit amet volutpat quam. Proin non erat nisi. Aenean fringilla lacus quis bibendum ullamcorper. Ut iaculis risus id neque imperdiet, ut pulvinar nulla pretium. In blandit dapibus bibendum. Mauris sed ex velit. Duis dignissim tempor tincidunt. Nunc lacinia commodo vestibulum.`,
         lastUpdated: '3-5-26',
         author: 'Laura Grace',
         likes: "5"
     },
-    'cast-off': {
+    'cast-on-crochet': {
         id: 2,
-        slug: 'cast-off',
-        title: 'Casting Off for Knitting',
-        description: 'Learn how to cast off to finish your knitting project.',
+        slug: 'cast-on-crochet',
+        title: 'Casting On for Crochet',
+        description: 'Learn how to cast on to start your crochet project.',
         textContent: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a mauris quis libero elementum pretium sit amet eu justo. Pellentesque id mauris ac nisi euismod laoreet. Morbi velit ligula, imperdiet ut sem vel, condimentum placerat arcu. Sed ligula dolor, venenatis a aliquet nec, molestie nec eros. Praesent tempor nulla ut tincidunt ultricies. Nunc mollis facilisis massa, quis ullamcorper nisi iaculis vitae. Nunc risus magna, fringilla eleifend leo eu, pulvinar laoreet leo. Ut tempor ultrices mauris, sit amet viverra elit ornare id. Nulla facilisi. Praesent sollicitudin tincidunt eros, et rhoncus velit egestas sed. Nam vitae nulla et magna tristique vestibulum at in purus. Mauris interdum pulvinar nunc. Vestibulum vitae ante viverra, pharetra orci nec, tincidunt felis. Duis consectetur a felis quis maximus. Fusce dignissim dolor ac ex condimentum malesuada. Fusce varius sollicitudin rhoncus.\nIn vel enim mi. Cras facilisis enim eu tempor porta. Sed malesuada elit non imperdiet semper. Duis quis enim elementum, tempus ipsum eget, finibus dolor. Phasellus vulputate condimentum purus, et elementum turpis vehicula et. Phasellus quis tempus risus, ut facilisis libero. Donec rhoncus enim interdum erat vulputate ultrices. Cras dapibus sed nisl suscipit bibendum. Etiam id elit ut ex feugiat pretium et sit amet felis. Nunc auctor vestibulum est, ac ultrices elit rutrum at. Etiam sit amet dui massa. Nam maximus convallis lacinia.\nVivamus tempus urna id luctus fringilla. Pellentesque nec imperdiet ante. Morbi vel tincidunt metus. Vestibulum ac ante sit amet enim consequat iaculis. In eu purus sit amet felis fermentum aliquet a vitae urna. Nulla condimentum non felis et malesuada. Sed a urna congue, efficitur dolor et, mattis erat. Curabitur vitae nunc porttitor, viverra erat eu, blandit eros. Praesent sodales vitae sapien id egestas.`,
         lastUpdated: '3-2-26',
         author: 'Lucy Jiang',
@@ -41,4 +41,11 @@ const getTutorialBySlug = (tutorialSlug) => {
     return tutorials[tutorialSlug] || null;
 };
 
-export { getAllTutorials, getTutorialBySlug };
+const separateTutorialParagraphs = (tutorialSlug) => {
+    const tutorial = getTutorialBySlug(tutorialSlug);
+
+    const paragraphs = tutorial.textContent.split('\n');
+    return paragraphs;
+};
+
+export { getAllTutorials, getTutorialBySlug, separateTutorialParagraphs };
